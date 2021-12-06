0e408651cb2a838a9d429f03e91c5dc65fb51134
Igor Anikin <igoranik054@gmail.com>
2021-12-06 10:02:08 +0200

    Added random generation of movie name

--- scarfilmparser.py
+++ scarfilmparser.py
@@ -1,12 +1,13 @@
 import re
 import requests
+from numpy import random
 from bs4 import BeautifulSoup
 
 URL = 'https://scarfilm.org/rekomenduju-k-prosmotru/'
 
 html_text = requests.get(URL).text
 soup = BeautifulSoup(html_text, 'html.parser')
-contentTable = soup.find('div', { "class" : "post-body-inner"})
+contentTable = soup.find('div', {"class": "post-body-inner"})
 
 titles = contentTable.find_all("strong")
 
@@ -16,3 +17,8 @@ for i in titles:
     f.write(i.get_text() + '\n')
 
 f.close()
+
+size = len(titles)
+rand_num = random.randint(0, size)
+
+print(titles[rand_num].get_text())
